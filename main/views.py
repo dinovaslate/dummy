29e3bcdad9d76a4af8bc3e1bdeb78ae6b46cddd3
Haekal Dinova <dinovasala3@gmail.com>
2025-09-03 15:45:41 +0700

    tutorial 2

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A'
+        'npm' : '2406352424',
+        'name': 'Haekal Alexander Dinova',
+        'class': 'PBP C'
     }
 
     return render(request, "main.html", context)
